f0557e079aef2c861392e575bda5edfbb1d51a76
amlaksil <silamlakdesye@gmail.com>
2023-09-27 03:31:40 +0300

    Implement handleProfileSignup function

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
