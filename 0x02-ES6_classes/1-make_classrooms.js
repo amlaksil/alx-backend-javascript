2c4ff9bc780430123eb87a7981da2c9100a50439
amlaksil <silamlakdesye@gmail.com>
2023-09-30 03:56:53 +0300

    Replace the ++ operator with the += 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,7 +4,7 @@ export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
   const rooms = [];
 
-  for (let i = 0; i < roomSizes.length; i++) {
+  for (let i = 0; i < roomSizes.length; i += 1) {
     const room = new ClassRoom(roomSizes[i]);
     rooms.push(room);
   }
