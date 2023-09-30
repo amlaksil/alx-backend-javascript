5eb1c08edb4ca5c5c88186528de6462e6d369363
amlaksil <silamlakdesye@gmail.com>
2023-09-30 05:46:26 +0300

    Implement a class named Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+const Currency = require('./3-currency');
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class.');
+    }
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price information
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Amount and conversion rate must be numbers.');
+    }
+    return amount * conversionRate;
+  }
+}
+
+module.exports = Pricing;
