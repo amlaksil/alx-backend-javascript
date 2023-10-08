33a82605ce699ff62d6fdc2eb7fb6155535bf138
amlaksil <silamlakdesye@gmail.com>
2023-10-08 05:42:22 +0300

    refactor: Update cleanSet function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return ('');
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
 
-  const strList = [...set].filter((str) => str.startsWith(startString));
-  return strList.join('-').split(startString).join('');
+  const strList = [...set]
+    .filter((str) => str.startsWith(startString))
+    .map((str) => str.substring(startString.length))
+    .join('-');
+
+  return strList;
 }
