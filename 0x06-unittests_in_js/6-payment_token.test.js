63145686b1f71577c449c667d3d80c363d101ff5
amlaksil <silamlakdesye@gmail.com>
2023-11-24 03:06:55 +0300

    Create getPaymentTokenFromAPI function and write a test suite

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with the correct data when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(done);
+  });
+});
