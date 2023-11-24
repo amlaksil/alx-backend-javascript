142f4dbcd3165945de3231887dbd483b39d65382
amlaksil <silamlakdesye@gmail.com>
2023-11-24 04:24:52 +0300

    Refactor the code

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -9,25 +9,33 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get('/cart/:id([0-9]+)', (req, res) => {
   const { id } = req.params;
   res.send(`Payment methods for cart ${id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-  const paymentMethods = {
+  res.json({
     payment_methods: {
       credit_cards: true,
       paypal: false
     }
-  };
-
-  res.json(paymentMethods);
+  });
 });
 
 app.post('/login', (req, res) => {
   const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  if (!userName) {
+    res.status(400).json({ error: 'userName is required' });
+  } else {
+    res.send(`Welcome ${userName}`);
+  }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,67 +2,73 @@ const request = require('request');
 const { expect } = require('chai');
 
 describe('Index Page', () => {
-	it('should return status code 200', (done) => {
+  it('should return status code 200', (done) => {
     request.get('http://localhost:7865', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done();
     });
+  });
+
+  it('should return the correct result', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
 });
 
-describe('/login Endpoint', () => {
-  it('should return status code 200 and welcome message', (done) => {
-    const userData = {
-      userName: 'JohnDoe'
-    };
-
-    request.post({
-      url: 'http://localhost:7865/login',
-      json: true,
-      body: userData
-    }, (error, response, body) => {
-			if (error) {
-				done(error);
-				return;
-			}
+describe('Cart Page', () => {
+  it('should return status code 200 when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('Welcome JohnDoe');
       done();
     });
   });
 
-  // Additional test cases for the /login endpoint
+  it('should return status code 404 when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
 
-  // ...
+  it('should return the correct result when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/456', (error, response, body) => {
+      expect(body).to.equal('Payment methods for cart 456');
+      done();
+    });
+  });
 });
 
-describe('/available_payments Endpoint', () => {
-  it('should return status code 200 and payment methods object', (done) => {
-    request.get('http://localhost:7865/available_payments', (error, response, body) => {
-		if (error) {
-			done(error);
-			return;
-			}
-      expect(response.statusCode).to.equal(200);
-
-			let parsedBody;
-      try {
-        parsedBody = JSON.parse(body);
-      } catch (parseError) {
-        done(parseError);
-        return;
+describe('Login Page', () => {
+  it('should return status code 200 and welcome message', (done) => {
+    const options = {
+      url: 'http://localhost:7865/login',
+      method: 'POST',
+      json: {
+        userName: 'John'
       }
+    };
 
-      expect(parsedBody).to.deep.equal({
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome John');
+      done();
+    });
+  });
+});
+
+describe('Available Payments Page', () => {
+  it('should return status code 200 and available payment methods', (done) => {
+    request.get('http://localhost:7865/available_payments', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal(JSON.stringify({
         payment_methods: {
           credit_cards: true,
           paypal: false
         }
-      });
+      }));
       done();
-			});
+    });
   });
-
-  // Additional test cases for the /available_payments endpoint
-
-  // ...
 });
